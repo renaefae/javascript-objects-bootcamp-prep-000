9bf9f3579f0dd2b75e611ab092dd2d75b6326352
Naenae L <oneprincesslea@gmail.com>
2018-11-13 17:47:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,8 @@ function updatePlaylist (playlist, artist, song){
  return object.assign (playlist, {michaelJackson: "Thriller"});
 };
 function removeFromPlaylist (playlist, artist, song){
-  return delete playlist.misterFelipe; 
+  return delete playlist.misterFelipe;
 }
 console.log(playlist);
-console.log(updatePlaylist);
+console.log(updatePlaylist); 
+console.log (removeFromPlaylist); 
